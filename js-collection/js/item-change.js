70a58915da895722a84a6141a7bb1f8440511952
fukuzawa-shinji <fukuzawa45@yahoo.co.jp>
2020-09-04 16:35:20 +0900

    test00

---
+++ js-collection/js/item-change.js
@@ -0,0 +1,9 @@
+var thumbs = document.querySelector(".thumb-img").children;
+function changeImage(event){
+document.querySelector(".m-img").src = event.children[0].src
+
+for(let i=0; i<thumbs.length;i++){
+  thumbs[i].classList.remove("active");
+}
+event.classList.add("active");
+}
